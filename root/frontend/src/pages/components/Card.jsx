2af0b292c857af2258da8b54ddc9cd7c06e369b2
Daniel Simboli <dansimboli@gmail.com>
2021-07-07 12:45:28 -0400

    Improve card display

--- root/frontend/src/pages/components/Card.jsx
+++ root/frontend/src/pages/components/Card.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   card: {
+    alignItems: 'flex-start',
     justifyContent: 'flex-start',
     marginTop: '10px',
+    borderRadius: '8px',
+    minHeight: '60px',
+    '&:hover': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+  title: {
+    paddingTop: '1px',
+    paddingLeft: '2px',
   },
 }));
 
@@ -14,7 +25,9 @@ const Card = (props) => {
   const classes = useStyles();
   return (
     <ButtonBase className={classes.card}>
-      {props.title}
+      <Typography variant="subtitle2" className={classes.title}>
+        {props.title}
+      </Typography>
     </ButtonBase>
   );
 };
