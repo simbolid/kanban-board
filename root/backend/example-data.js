b626454bbda186508c2c96054ee9d51004c6df06
Daniel Simboli <dansimboli@gmail.com>
2021-07-08 08:25:16 -0400

    Install packages and create placeholder data

---
+++ root/backend/example-data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    title: 'Product Backlog',
+    cards: [
+      { title: 'Use fontsources to add Roboto (or Inter) locally' },
+      { title: 'Use database IDs as keys for lists' },
+    ],
+  },
+  {
+    title: 'In Progress',
+    cards: [
+      { title: 'Implement GET operations' },
+      { title: 'Store data in a MongoDB cluster' },
+    ],
+  },
+  {
+    title: 'Done',
+    cards: [
+      { title: 'Implement basic column functionality' },
+      { title: 'Consistent column sizing' },
+    ],
+  },
+];
+
+module.exports = data;
