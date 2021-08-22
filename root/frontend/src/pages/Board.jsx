87198ebc9fe8d335458316b62d4e8717c37f5e88
das <dansimboli@gmail.com>
2021-08-22 17:16:34 -0400

    Move components used only by Board to a 'board' directory

--- root/frontend/src/pages/Board.jsx
+++ root/frontend/src/pages/Board.jsx
@@ -4,8 +4,8 @@ import Box from '@material-ui/core/Box';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import NavigationInterface from '../components/Navigation';
-import Column from '../components/Column';
-import ButtonToTextField from '../components/ButtonToTextField';
+import Column from '../components/board/Column';
+import ButtonToTextField from '../components/board/ButtonToTextField';
 import boardService from '../services/boards';
 
 const useStyles = makeStyles((theme) => ({
