87b8209213433ef5a5ab14e0e89433fd92d51745
das <dansimboli@gmail.com>
2021-09-01 08:40:10 -0400

    Disable react strict mode
    The warning "findDOMNode is deprecated in strict mode" was being called whenever an MUI modal was clicked. This is an issue with MUI, so I don't need to see this warning.

--- root/frontend/src/index.jsx
+++ root/frontend/src/index.jsx
@@ -4,10 +4,10 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </>,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
 );
