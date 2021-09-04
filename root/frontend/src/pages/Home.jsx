4d7f79987b765c77267ba80855212944dd4c5268
das <dansimboli@gmail.com>
2021-09-03 21:21:57 -0400

    Update homepage

--- root/frontend/src/pages/Home.jsx
+++ root/frontend/src/pages/Home.jsx
@@ -9,17 +9,12 @@ import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
-    backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
   },
   heroButton: {
     marginTop: theme.spacing(4),
     display: 'flex',
     justifyContent: 'center',
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
   },
 }));
 
@@ -30,24 +25,23 @@ const Home = () => {
     <>
       <AppBar position="relative" elevation={0}>
         <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
+          <Typography variant="h6" noWrap>
             Kanban Bonsai
           </Typography>
         </Toolbar>
       </AppBar>
       <main>
-        {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
               Kanban Bonsai
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              A minimalist project management solution.
+              Basic Kanban boards for individual projects
             </Typography>
             <div className={classes.heroButton}>
               <Button
-                variant="outlined"
+                variant="contained"
                 size="large"
                 color="secondary"
                 component={Link}
